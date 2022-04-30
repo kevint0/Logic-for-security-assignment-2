a5619e1a011839cf0c557e59b3fd106d932d96f9
kevin <kevinthorfleming@gmail.com>
2022-04-30 23:04:13 +0200

    With User class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from User import User
 
 print("enter login")
 username = input()
@@ -14,13 +15,14 @@ def login(username, password):
             print(accountData.iloc[j,1])
             if username == i:
                 if accountData.iloc[j,1] == password:
+                    currentUser = User(username, accountData.iloc[j,2])
                     print("you are logged in as " + username)
-                    return username
+                    return currentUser
                 else:
                     print("password does not match")
-                    return "null"
+                    return
             j += 1
         print("username not found")
-        return "null"
+        return
 
 login(username, password)
